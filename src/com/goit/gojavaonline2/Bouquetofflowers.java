2898f2570dd04fa1cb97d65f88d8e975a595e367
DenRib <denrib12@gmail.com>
2016-03-12 21:39:27 +0200

    Initial commit

---
+++ src/com/goit/gojavaonline2/Bouquetofflowers.java
@@ -0,0 +1,12 @@
+package com.goit.gojavaonline2;
+
+import java.util.List;
+
+/**
+ * Created by Денис Риб on 12.03.2016.
+ */
+public class Bouquetofflowers {
+    List<Rose> roses;
+    List<Aster> asters;
+    List<Chamomile> chamomiles;
+}
